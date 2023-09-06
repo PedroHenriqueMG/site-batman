2159bcf2792fc08fe6dca6b32e9507c6ccf9f0b2
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-09-06 09:47:47 -0300

    add actor cards

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,8 +1,4 @@
-'use client';
-
 import Header from '@/app/components/header/header'
-import { CldVideoPlayer } from 'next-cloudinary'
-import 'next-cloudinary/dist/cld-video-player.css';
 import s from './home.module.css'
 
 export default function Index() {
@@ -12,7 +8,11 @@ export default function Index() {
           <div id={`${s.banner}`}></div>
             <div id={`${s.trailerContainer}`}>
                 <div className={s.content}>
-                    <CldVideoPlayer width='400' height='270' src='../../assets/video.mp4' />
+                    <video width="50%" height="240" controls>
+                        <source src="/video.mp4" type="video/mp4" />
+                        Seu navegador não suporta a tag de vídeo.
+                    </video>
+
                     <div id={s.sinopse}>
                         <p className={s.description}>
                             The Batman é um thriller cheio de ação que retrata o Batman em seus primeiros anos, lutando para equilibrar a raiva com a retidão 
@@ -23,6 +23,13 @@ export default function Index() {
                         </p>
                         <button className={s.button}>Comprar ingresso</button>
                     </div>
+                </div>
+            </div>
+            <div className={s.actor_cards_container}>
+                <div className={s.cardsContent}>
+                    <div className={`${s.cards}  ${s.banner_1}`}>Robert Pattison - Batman</div>
+                    <div className={`${s.cards}  ${s.banner_2}`}>Zoe Kravitz - Mulher Gato</div>
+                    <div className={`${s.cards}  ${s.banner_3}`}>Jeffrey Wright - Gordon</div>
                 </div>
             </div>
         </>
